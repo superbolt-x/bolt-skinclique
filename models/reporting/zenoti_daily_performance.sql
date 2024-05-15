6d511ba609696cf981cbbdb6ad8dbb1e1e9c7ae0
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2024-05-15 14:11:20 -0400

    Update zenoti_daily_performance.sql
    future_appointments_booked

--- models/reporting/zenoti_daily_performance.sql
+++ models/reporting/zenoti_daily_performance.sql
@@ -15,12 +15,14 @@ WITH initial_data as
       '{{date_granularity}}' as date_granularity,
       {{date_granularity}} as date,
       service_name, service_category, product_name, product_category,
-      COUNT(appointment_id) as appointments_requested,
-      COUNT(CASE WHEN appointment_status IN ('Confirmed','Closed','Open') THEN appointment_id END) as appointments_booked,
-      COUNT(CASE WHEN appointment_cancel_or_no_show_status = 'Fulfilled Appointment' THEN appointment_id END) as visits,
+      COUNT(DISTINCT appointment_id) as appointments_requested,
+      COUNT(DISTINCT CASE WHEN appointment_status IN ('Confirmed','Checkin','Open','Closed') THEN appointment_id END) as all_appointments_booked,
+      COUNT(DISTINCT CASE WHEN appointment_status IN ('Open') THEN appointment_id END) as future_appointments_booked,
+      COUNT(DISTINCT CASE WHEN appointment_status IN ('Closed') AND item_name !~* 'book' THEN appointment_id END) as visits,
       COALESCE(SUM(total_adjusted_revenue),0) as total_adjusted_revenue,
       COALESCE(SUM(total_revenue),0) as total_revenue
       FROM initial_data
+      AND center_name !~* 'training' AND service_category !~* 'perks'
       GROUP BY 1,2,3,4,5,6
       {% if not loop.last %}UNION ALL
       {% endif %}
@@ -34,7 +36,8 @@ SELECT
   product_name, 
   product_category,
   appointments_requested,
-  appointments_booked,
+  all_appointments_booked,
+  future_appointments_booked,
   visits,
   total_adjusted_revenue,
   total_revenue
